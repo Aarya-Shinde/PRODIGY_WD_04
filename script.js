1e375ae39cd4d6208b84cda7a27e9db04998f73d
Aarya Shinde <148617878+Aarya-Shinde@users.noreply.github.com>
2024-11-19 20:21:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ // Simulate smooth transition on page reload
+ window.addEventListener('beforeunload', function () {
+    document.body.style.backgroundColor = '#fff'; // White background during transition
+});
